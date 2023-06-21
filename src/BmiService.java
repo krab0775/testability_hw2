e3db8abdc4e01041789c8837c732bb4529e45f3d
krab0775 <krab0775@gmail.com>
2023-06-21 13:51:13 +0700

    edit BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+
+    public int calculate(double heightM, int weightKg) {
+        double result;
+        if (weightKg > 0) {
+            result = weightKg / (heightM * heightM);
+        } else {
+            result = 0;
+        }
+        return (int) result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+        double heightM = 1.9;
+        int weightKg = 87;
+        int bmi = service.calculate(heightM, weightKg);
+        System.out.println("Индекс массы тела: "+bmi);
+    }
 }
